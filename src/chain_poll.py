803f5d15fc34f3c81cfa2701c565b79605459222
Neel Gokhale <neel@cohere.com>
2024-08-21 18:40:53 -0400

    improved chain_poll.py

--- src/chain_poll.py
+++ src/chain_poll.py
@@ -2,11 +2,13 @@
 
 import os
 import re
-import glob
 import pandas as pd
 
+from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor
+from ratelimit import limits, sleep_and_retry
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Literal
 from tqdm import tqdm
 from ast import literal_eval
 from langchain_core.output_parsers import JsonOutputParser
@@ -63,6 +65,9 @@ class Records(BaseModel):
     records: List[Record] = Field(description="A list of Record JSONs that captures diverse reasonings and judgements for a given Response")
 
 
+SourceType = Literal["drop", "hotpot", "msmarco", "narrativeqa"]
+
+
 class ChainPollEval(BaseEval):
     """
     Chain Poll evaluation class
@@ -76,15 +81,22 @@ class ChainPollEval(BaseEval):
             api_key=self.api_key, model=model
         )
 
-    def load_data(self, *, path: str | os.PathLike, samples: Optional[int]=None) -> None:
-        if samples is not None:
+    def load_data(self, *, path: str | os.PathLike, samples: Optional[int]=None, subset: Optional[SourceType]=None) -> None:
+        if samples is not None and subset is not None:
+            self.dataset = pd.read_csv(path)
+            self.dataset = self.dataset[self.dataset["source"] == subset.lower()].sample(samples)
+        if samples is not None and subset is None:
             self.dataset = pd.read_csv(path).sample(samples)
-        else:
+        if samples is None and subset is not None:
+            self.dataset = pd.read_csv(path)
+            self.dataset = self.dataset[self.dataset["source"] == subset.lower()]
+        if samples is None and subset is None:
             self.dataset = pd.read_csv(path)
         cols_to_keep = [
             "unrendered_prompt",
             "generations",
             "prompted_evaluator_score_command-r_generations",
+            "source",
         ]
         drop_columns = [col for col in self.dataset.columns if col not in cols_to_keep]
         self.dataset = self.dataset.drop(columns=drop_columns)
@@ -93,10 +105,10 @@ class ChainPollEval(BaseEval):
         self.dataset["question"] = self.dataset["unrendered_prompt"].apply(self._extract_question)
         self.dataset["unrendered_prompt"] = self.dataset["unrendered_prompt"].apply(self._extract_unrendered_prompt)
         self.dataset = self.dataset.dropna(subset=["unrendered_prompt"])
-        self.dataset["prompted_evaluator_score_command-r_generations"] = \
-            self.dataset["prompted_evaluator_score_command-r_generations"].apply(self._extract_evaluator_score)
         self.dataset["generations"] = self.dataset["generations"].apply(self._extract_generation)
         self.dataset["judgement_prompt"] = self.dataset.apply(self._create_judgement_prompt, axis=1)
+        self.dataset["prompted_evaluator_score_command-r_generations"] = \
+            self.dataset["prompted_evaluator_score_command-r_generations"].apply(self._extract_evaluator_score)
         self.dataset = self.dataset.rename(columns={
             "generations": "response",
             "unrendered_prompt": "context",
@@ -104,31 +116,33 @@ class ChainPollEval(BaseEval):
         })
         self.dataset["doc_type"] = self.dataset["context"].apply(lambda x: "multi" if len(x) > 1 else "single")
         
-    def run_eval(self) -> None:
+    def run_eval(self, parallel: bool=False) -> None:
         # generate judgement for each record
         parser = JsonOutputParser(pydantic_object=Records)
         cp_prompt = PromptTemplate(
-            template=CP_PROMPT + "\n## Format instructions\n{format_instructions}",
-            input_variables=["context", "response"],
+            template="{prompt}" + "\n## Format instructions\n{format_instructions}",
+            input_variables=["prompt"],
             partial_variables={"format_instructions": parser.get_format_instructions()},
         )
         self.judgement_chain = cp_prompt | self.model | parser
-
         self.dataset["record"] = None
-        record = []
-        cp_score = []
-        for index in tqdm(list(self.dataset["judgement_prompt"].index), desc='Generating judgements', unit="row"):
-            # generate judgement
-            _record = self._generate_judgement(
-                context=self.dataset.loc[index, "context"],
-                response=self.dataset.loc[index, "response"],
-            )
-            record.append(_record)
-            # calculate Chain Poll score
-            cp_score.append(self._calculate_cp_score(_record))
+
+        if parallel:
+            records = self._generate_parallel_judgement()
+        else:
+            records = []
+            for index in tqdm(list(self.dataset["judgement_prompt"].index), desc='Generating judgements', unit="row"):
+                # generate judgement
+                _record = self._generate_judgement(
+                    prompt=self.dataset.loc[index, "judgement_prompt"]
+                )
+                records.append(_record)
+
         # add to dataset
-        self.dataset["cp_score"] = cp_score
-        self.dataset["record"] = record
+        self.dataset["record"] = records
+
+        # calculate Chain Poll score
+        self.dataset["cp_score"] = self.dataset.apply(self._calculate_cp_score, axis=1)
 
         # apply ragas evaluation
         ragas_args = {
@@ -147,9 +161,10 @@ class ChainPollEval(BaseEval):
             path (str | PathLike): path to a file or directory. If directory path provided, generates based on latest
                 file. specific file can be provided to overwrite
         """
+        cols = ["record_no", "source", "doc_type", "question", "response", "score", "cp_score", "ragas_score"]
         # check if a jsonl file is provided
         if os.path.isfile(path) and path.endswith(".jsonl"):
-            return pd.read_json(path, lines=True)
+            return pd.read_json(path, lines=True)[cols]
         # check if a directory path is provided
         if not os.path.isdir(path):
             raise FileNotFoundError(f"Directory or file {path} does not exist")
@@ -158,7 +173,7 @@ class ChainPollEval(BaseEval):
             raise FileNotFoundError("No runs exist. Run and save evaluation before running the report")
         run_paths = [os.path.join(path, f) for f in runs]
         latest_run_path = max(run_paths, key=os.path.getmtime)
-        return pd.read_json(latest_run_path, lines=True)
+        return pd.read_json(latest_run_path, lines=True)[cols]
 
     def save_records(self, *, save_dir: str | os.PathLike) -> None:
         if self.dataset is None:
@@ -167,12 +182,25 @@ class ChainPollEval(BaseEval):
             raise FileNotFoundError(f"Directory {save_dir} does not exist")
         timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         path = os.path.join(save_dir, "run_" + timestamp + ".jsonl")
-        self.dataset[
-            ["record_no", "doc_type", "question", "response", "score", "cp_score", "ragas_score"]
-        ].to_json(path, lines=True, orient="records")
+        self.dataset.to_json(path, lines=True, orient="records")
 
-    def _calculate_cp_score(self, record: list[dict]) -> float:
-        yes_records = len([item for item in record["records"] if item["judgement"].lower() == "yes"])
+    def get_record_info(self, record_no: int) -> list:
+        if self.dataset is None:
+            raise ValueError("Load dataset first")
+        row = self.dataset.loc[self.dataset["record_no"] == record_no]
+        question = row["question"].values
+        response = row["response"].values
+        records = row["record"].values["records"]
+        source = row["source"].values
+        print(f"QUESTION - {question}")
+        print(f"RESPONSE - {response}")
+        print(f"SOURCE - {source}")
+        for record in records:
+            pprint(record)
+        return records
+
+    def _calculate_cp_score(self, x: pd.Series) -> float:
+        yes_records = len([item for item in x["record"]["records"] if item["judgement"].lower() == "yes"])
         # calculate chain poll score
         cp_score = yes_records / self.TOTAL_RUNS
         return cp_score
@@ -187,19 +215,57 @@ class ChainPollEval(BaseEval):
         scores = evaluate(dataset, llm=self.model, metrics=[faithfulness]).to_pandas()
         return scores["faithfulness"].to_list()
 
-    def _generate_judgement(self, context: str, response: str) -> list:
+    def _generate_judgement(self, prompt: str) -> dict:
         judgement_params = {
-            "context": context,
-            "response": response,
+            "prompt": prompt
         }
         judgement_record = self.judgement_chain.invoke(
             judgement_params
         )
         return judgement_record
+    
+    def _generate_parallel_judgement(self) -> list:
+        records = []
+        # define rate limit
+        @sleep_and_retry
+        @limits(calls=5, period=100)
+        def rate_limited_invoke_call(prompt: str) -> dict:
+            
+            return self.judgement_chain.invoke(
+                {
+                    "prompt": prompt
+                }
+            )
+
+        MAX_THREADS = 5
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            # create list of futures
+            futures = [
+                executor.submit(
+                    rate_limited_invoke_call,
+                    prompt=prompt,
+                )
+                for prompt in self.dataset.loc[:, "judgement_prompt"]
+            ]
+
+            # run futures w progress bar
+            with tqdm(total=len(futures), desc="Generating judgements") as pbar:
+                for future in futures:
+                    _record = future.result()
+                    records.append(_record)
+                    pbar.update(1)
+        return records
 
     def _create_judgement_prompt(self, x: pd.Series) -> str:
+        context = ""
+        if len(x["unrendered_prompt"]) == 1:
+            context = x["unrendered_prompt"][0]
+        else:
+            for doc in x["unrendered_prompt"]:
+                context += f"- {doc}\n"
+
         prompt = CP_PROMPT.format(
-            context = x["unrendered_prompt"],
+            context = context,
             response = x["generations"]
         )
         return prompt
